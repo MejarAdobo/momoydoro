8bdaffda46a09d544b1e475970f3a1039f87547e
MejarAdobo <momoy.dev@proton.me>
2025-12-22 15:14:16 -0800

    recreate a new svelte app again

--- src/lib/components/Timer.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    export let timerLength: number;
-    export let timerName: string;
-</script>
-
-<div>
-    <h2>{timerName}</h2>
-    <p>{timerLength} minutes</p>
-</div>
--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import Timer from "$lib/components/Timer.svelte";
-    let timerLength = 25;
-    let timerName = "Pomodoro";
-</script>
-
-<Timer {timerLength} {timerName} />
